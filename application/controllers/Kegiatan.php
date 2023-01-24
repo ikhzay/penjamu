169ebe72914040242c43e1bd3b0fcab7d4049546
ikhzay <ikhzay@gmail.com>
2023-01-24 14:12:35 +0800

    Update Kegiatan

--- application/controllers/Kegiatan.php
+++ application/controllers/Kegiatan.php
@@ -43,10 +43,37 @@ class Kegiatan extends RestController {
 
         $this->response([
             'status' => 'success',
-            'message' => 'Count Of Kegiatan',
+            'message' => 'Count Of Tingkat',
             'local' => $local,
             'nasional' => $nasional,
             'internasional' => $inter,
+        ], 200);
+    }
+
+    public function bidang_get(){
+        $kegiatan = $this->db->get_where('tb_1a',array('prodi_kode'=>'552011'))->result();
+        $pendidikan=0;
+        $penelitian=0;
+        $lainnya=0;
+
+        foreach($kegiatan as $item){
+            if($item->bidang == 'P1'){
+                $pendidikan+=1;
+            }
+            else if($item->bidang == 'P2'){
+                $penelitian+=1;
+            }
+            else {
+                $lainnya+=1;
+            }
+        }
+
+        $this->response([
+            'status' => 'success',
+            'message' => 'Count Of Bidang',
+            'pendidikan' => $pendidikan,
+            'penelitian' => $penelitian,
+            'lainnya' => $lainnya,
         ], 200);
     }
 }
